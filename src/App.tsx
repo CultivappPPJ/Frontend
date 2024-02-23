bc34db19f811ccab835040bb97eb3dedb88d7282
Sebastián <sebastian.h.neira@outlook.com>
2024-02-22 23:28:59 -0300

    Se crea los formularios login y register

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { Button } from "@mui/material";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Button variant="outlined">Hello world</Button>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,9 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-import "./index.css";
 import { CssBaseline } from "@mui/material";
+
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
