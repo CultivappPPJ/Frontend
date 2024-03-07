1be262ac3355a9cb6979bac94c7c3fe324095911
Sebastián <sebastian.h.neira@outlook.com>
2024-03-07 09:53:15 -0300

    Cambios en el llamado de la paginación

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,60 +12,54 @@ import {
   CardMedia,
   CircularProgress,
 } from "@mui/material";
-import Navbar from "../components/Navbar";
 import Pagination from "@mui/material/Pagination";
 import { useEffect, useState } from "react";
 import TerrainCard from "../components/TerrainCard";
 import "../styles/styles.css";
 import axios from "axios";
-
-interface Terrain {
-  id: number;
-  area: string;
-  soilType: string;
-  plantType: string;
-  photo: string;
-  email: string;
-  remainingDays: number;
-  forSale: boolean;
-  fullName: string;
-}
-
-const ITEMS_PER_PAGE = 3;
+import { TerrainResponse } from "../types";
 
 export default function Home() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [terrainsData, setTerrainsData] = useState<Terrain[]>([]);
-  const [selectedTerrain, setSelectedTerrain] = useState<Terrain | null>(null);
+  const [terrainsData, setTerrainsData] = useState<TerrainResponse[]>([]);
+  const [selectedTerrain, setSelectedTerrain] =
+    useState<TerrainResponse | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [paginationInfo, setPaginationInfo] = useState({
+    currentPage: 0,
+    totalPages: 1,
+  });
+
+  // Realizar la solicitud axios al back end
+  const fetchData = async (pageNumber = 0) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/api/v1/terrain/all?page=${pageNumber}&size=6`
+      );
+      const data = response.data;
+      setTerrainsData(data.content); // Actualizar el estado con los datos obtenidos de la API
+      setPaginationInfo({
+        currentPage: response.data.pageable.pageNumber + 1,
+        totalPages: response.data.totalPages,
+      });
+      setLoading(false);
+    } catch (error) {
+      console.error("Error al obtener datos de la API:", error);
+    }
+  };
 
   useEffect(() => {
-    // Realizar la solicitud axios al back end
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:8080/api/v1/terrain/all"
-        );
-        const data = response.data;
-        setTerrainsData(data.content); // Actualizar el estado con los datos obtenidos de la API
-        setLoading(false);
-      } catch (error) {
-        console.error("Error al obtener datos de la API:", error);
-      }
-    };
-
     fetchData();
   }, []); // El array vacío asegura que la solicitud se realice solo una vez al montar el componente
 
   const handleChangePage = (
     event: React.ChangeEvent<unknown>,
-    value: number
+    newPage: number
   ) => {
-    setCurrentPage(value);
+    fetchData(newPage - 1);
   };
 
-  const handleCardClick = (terrain: Terrain) => {
+  const handleCardClick = (terrain: TerrainResponse) => {
     setSelectedTerrain(terrain);
     setIsModalOpen(true);
   };
@@ -73,13 +67,6 @@ export default function Home() {
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
-
-  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-  const filteredTerrains = terrainsData.filter((terrain) => terrain.forSale);
-  const paginatedTerrains = filteredTerrains.slice(
-    startIndex,
-    startIndex + ITEMS_PER_PAGE
-  );
 
   return (
     <>
@@ -93,11 +80,11 @@ export default function Home() {
           // El resto del contenido cuando los datos están cargados
           <>
             <Typography variant="h6" className="welcome-message">
-              {paginatedTerrains.length > 0
+              {terrainsData.length > 0
                 ? "¡Bienvenido a CultivApp! Cultivos disponibles para ventas:"
                 : "¡Bienvenido a CultivApp! Aún no hay cultivos a la venta"}
             </Typography>
-            {paginatedTerrains.length === 0 ? (
+            {terrainsData.length === 0 ? (
               <div className="no-crops-message">
                 <img
                   src="https://w0.peakpx.com/wallpaper/374/819/HD-wallpaper-green-field-sunset-clouds-countryside-farming-agriculture-landscape.jpg"
@@ -110,14 +97,13 @@ export default function Home() {
                 container
                 spacing={2}
                 justifyContent={
-                  paginatedTerrains.length === 1 ||
-                  paginatedTerrains.length === 2
+                  terrainsData.length === 1 || terrainsData.length === 2
                     ? "center"
                     : "flex-start"
                 }
                 alignItems="stretch"
               >
-                {paginatedTerrains.map((terrain) => (
+                {terrainsData.map((terrain) => (
                   <Grid item key={terrain.id} xs={12} md={6} lg={4}>
                     <Card
                       onClick={() => handleCardClick(terrain)}
@@ -129,10 +115,10 @@ export default function Home() {
                 ))}
               </Grid>
             )}
-            {paginatedTerrains.length > 0 && (
+            {terrainsData.length > 0 && (
               <Pagination
-                count={Math.ceil(filteredTerrains.length / ITEMS_PER_PAGE)}
-                page={currentPage}
+                count={paginationInfo.totalPages}
+                page={paginationInfo.currentPage}
                 onChange={handleChangePage}
                 color="primary"
                 size="large"
@@ -175,7 +161,7 @@ export default function Home() {
                 </DialogContentText>
               </DialogContent>
               <DialogActions className="dialog-actions">
-                <Button onClick={handleCloseModal} color="primary">
+                <Button onClick={handleCloseModal} color="error">
                   Cerrar
                 </Button>
               </DialogActions>
