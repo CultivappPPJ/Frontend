39ca93515ee68f6fbc456bafb6d3311b1b509bb6
marcialDiazFlores <marcialbenjamindiazflores@gmail.com>
2024-03-12 12:29:21 -0300

    Window Alert cambiado por SnackBar

--- src/components/ModalDeleteAccount.tsx
+++ src/components/ModalDeleteAccount.tsx
@@ -1,4 +1,5 @@
 import {
+  Alert,
   Box,
   Button,
   Dialog,
@@ -6,12 +7,15 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  Snackbar,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logout } from "../features/auth/authSlice";
+import { Container } from "@mui/system";
+import { useState } from "react";
 
 interface ModalDeleteAccountProps {
   openDialog: boolean;
@@ -27,6 +31,13 @@ const ModalDeleteAccount: React.FC<ModalDeleteAccountProps> = ({
 }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const handleCloseSnackbar = () => {
+    dispatch(logout());
+    setSnackbarOpen(false);
+    navigate("/signin");
+  };
   const handleDelete = async () => {
     try {
       const token = localStorage.getItem("token");
@@ -50,41 +61,56 @@ const ModalDeleteAccount: React.FC<ModalDeleteAccountProps> = ({
       );
 
       console.log("Respuesta de eliminación de cuenta:", response.data);
-      window.alert("Tu cuenta ha sido eliminada exitosamente.");
-      dispatch(logout());
-      navigate("/signin");
-      handleClose();
+      setSnackbarMessage("Cuenta eliminada exitosamente");
+      setSnackbarOpen(true);
     } catch (error) {
       console.error("Error al eliminar la cuenta:", error);
-      handleClose();
+      setSnackbarMessage("Cuenta eliminada exitosamente");
+      setSnackbarOpen(true);
     }
   };
 
   return (
-    <Dialog
-      open={openDialog}
-      onClose={handleClose}
-      aria-labelledby="alert-dialog-title"
-      aria-describedby="alert-dialog-description"
-    >
-      <DialogTitle id="alert-dialog-title">
-        {"Confirmar eliminación"}
-      </DialogTitle>
-      <Box display="flex" justifyContent="center" alignItems="center">
-        <DeleteIcon sx={{ fontSize: 60 }} />
-      </Box>
-      <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          ¿Estás seguro de que quieres eliminar tu cuenta (efecto permanente)?
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Cancelar</Button>
-        <Button onClick={handleDelete} autoFocus color="error">
-          Eliminar mi cuenta
-        </Button>
-      </DialogActions>
-    </Dialog>
+    <Container>
+      <Dialog
+        open={openDialog}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Confirmar eliminación"}
+        </DialogTitle>
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <DeleteIcon sx={{ fontSize: 60 }} />
+        </Box>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            ¿Estás seguro de que quieres eliminar tu cuenta (efecto permanente)?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancelar</Button>
+          <Button onClick={handleDelete} autoFocus color="error">
+            Eliminar mi cuenta
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={2000}
+        onClose={handleCloseSnackbar}
+      >
+        <Alert
+          onClose={handleCloseSnackbar}
+          severity="success"
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
+    </Container>
   );
 };
 
